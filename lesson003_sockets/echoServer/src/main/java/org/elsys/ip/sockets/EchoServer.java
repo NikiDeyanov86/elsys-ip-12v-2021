54d388ce913ca91a8ce9f22fdb47d3f602a45c5a
Ivaylo Mihaylov <ivaylo.mihaylov@botronsoft.com>
2021-10-04 14:55:41 +0300

    Lesson 003 - Sockets

---
+++ lesson003_sockets/echoServer/src/main/java/org/elsys/ip/sockets/EchoServer.java
@@ -0,0 +1,38 @@
+package org.elsys.ip.sockets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        while (true) {
+            String command = in.readLine();
+            out.println(command);
+        }
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        EchoServer server=new EchoServer();
+        server.start(6666);
+    }
+}
