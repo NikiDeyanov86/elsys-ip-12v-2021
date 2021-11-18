c7bde5433c45da79f2acef9570f2242675e10757
Ivaylo Mihaylov <ivaylo.mihaylov@botronsoft.com>
2021-11-18 18:31:56 +0200

    Lesson 010 Spring Web

---
+++ lesson010_springWeb/src/main/java/org/elsys/ip/springweb/OurController.java
@@ -0,0 +1,17 @@
+package org.elsys.ip.springweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class OurController {
+
+    @GetMapping
+    public String getString() {
+        return "Hello World";
+    }
+}
